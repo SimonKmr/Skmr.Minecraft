ddd76bb26b76e8da0072c410e92b9b7c0f3ea249
SimonKmr <darkfish070@gmail.com>
2023-11-09 22:01:54 +0100

    add comments

--- Skmr.Minecraft.Infrastructure/server_setup.sh
+++ Skmr.Minecraft.Infrastructure/server_setup.sh
@@ -1,1 +1,24 @@
-echo hello world
+# helpful article https://phoenixnap.com/kb/linux-set-environment-variable
+# install java (Amazons version)
+rpm --import https://yum.corretto.aws/corretto.key
+curl -L -o /etc/yum.repos.d/corretto.repo https://yum.corretto.aws/corretto.repo
+yum install -y java-17-amazon-corretto-devel.x86_64
+
+# adds a dedicated user for minecraft
+sudo adduser minecraft
+su
+
+# installs server (should be spigot)
+# should also rename from whatever version it is to "server.jar"
+# should also install the plugins i've written
+
+
+# installs server manager
+
+
+# sets variables
+MINECRAFT_SERVER = "Path to minecraft server"
+export MINECRAFT_SERVER
+
+# adds service
+# minecraft server
